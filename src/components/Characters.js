72076dcffe9e6418cdcb9d6cc0b6261e4eee9cdc
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-03-27 18:49:22 -0300

    Codigo base

---
+++ src/components/Characters.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Characters = ({characters = []}) => {
+  return (
+    <div className='row'>
+        {
+            characters.map((item, index) => (
+                <div key={index} className='col mb-4'>
+                    <div className='card' style={{maxWidth: "200px", minWidth: "250px"}}>
+                        <img src={item.image} alt="" />
+                        <div className='card-body'>
+                            <h5 className='card-title'>{item.name}</h5>
+                            <hr/>
+                            <p> Ubicación: {item.location.name}</p>
+                            <p> Especie: {item.species}</p>
+                            <p> Estatus: {item.status}</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            ))
+        }
+        
+
+    </div>
+  )
+}
+
+export default Characters
